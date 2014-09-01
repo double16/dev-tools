527a58d3c5ceb13a0a1b4bf9de5010ef244125ad
Patrick Double <pat@patdouble.com>
2014-09-01 11:14:35 -0500

    Add Sauce Labs - Sauce Connect scripts

---
+++ sauce-connect.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+#
+# Initiates Sauce Connect in the background for use by functional testing tools.
+# Requires the following environment variables:
+# SAUCE_LABS_USER
+# SAUCE_LABS_ACCESS_PASSWORD
+#
+if [ -z "${SAUCE_LABS_USER}" -o -z "${SAUCE_LABS_ACCESS_PASSWORD}" ]; then
+	echo "Environment variables SAUCE_LABS_USER and SAUCE_LABS_ACCESS_PASSWORD (aka API key) are required"
+	exit 1
+fi
+
+mkdir -p target || exit $?
+cd target || exit $?
+[ -f "sc-4.3-linux.tar.gz" ] || wget -nd https://saucelabs.com/downloads/sc-4.3-linux.tar.gz || exit $?
+tar xzvfp sc-4.3-linux.tar.gz || exit $?
+./sc-4.3-linux/bin/sc -u "${SAUCE_LABS_USER}" -k "${SAUCE_LABS_ACCESS_PASSWORD}" -f ~/sc-ready &
+C=0
+while [ ! -f ~/sc-ready -a $C -lt 30 ]; do sleep 5s; C=$(($C+1)); done
+
