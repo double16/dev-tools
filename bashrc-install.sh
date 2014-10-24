4b260d3ec090a258eabe8121b1addf2c94ff042e
Patrick Double <pat@patdouble.com>
2014-10-24 01:06:44 +0000

    Set PATH and add guard alias

--- bashrc-install.sh
+++ bashrc-install.sh
@@ -2,5 +2,7 @@
 
 cat >>"${HOME}/.bashrc" <<EOF
 export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64
+export PATH=\${PATH}:\${HOME}/node_modules/karma/bin/:\${HOME}/workspace/dev-tools:\${HOME}/node_modules/jshint/bin
+alias guard='./grailsw -reloading -Dgrails.nofork=true --echoOut -guard test-app integration:'
 EOF
 
